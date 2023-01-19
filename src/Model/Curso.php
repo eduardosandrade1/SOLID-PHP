fd0a86adaf263ccb378eecb72b2dbe20fd2acaa0
Eduardo Andrade <61837123+eduardosandrade1@users.noreply.github.com>
2023-01-19 18:24:31 -0300

    S and O letters applyed

---
+++ src/Model/Curso.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Alura\Solid\Model;
+
+class Curso implements Pontuavel
+{
+    private $nome;
+    private $videos;
+    private $feedbacks;
+
+    public function __construct(string $nome)
+    {
+        $this->nome = $nome;
+        $this->videos = [];
+        $this->feedbacks = [];
+    }
+
+    public function receberFeedback(FeedBack $feedback): void
+    {
+        $this->feedbacks[] = $feedback;
+    }
+
+    public function adicionarVideo(Video $video)
+    {
+        if ($video->minutosDeDuracao() < 3) {
+            throw new \DomainException('Video muito curto');
+        }
+
+        $this->videos[] = $video;
+    }
+
+    /** @return Video[] */
+    public function recuperarVideos(): array
+    {
+        return $this->videos;
+    }
+
+    public function recuperaPontuacao(): int
+    {
+        return 100;
+    }
+}
